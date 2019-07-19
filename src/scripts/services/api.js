ae0f7c971ea36ddda64ead166d357f9b3b51f46c
Jay <venzor@sg.uob>
2019-07-19 14:47:04 +0800

    # service api
    Signed-off-by: Jay <venzor@sg.uob>

--- src/scripts/services/api.js
+++ src/scripts/services/api.js
@@ -1,1 +1,22 @@
-import React from 'react';
+
+export const PostService = (api, postData) => {
+	let BaseUrl = api;
+	
+	return new Promise((resolve,reject)=>{
+		fetch(BaseUrl,{
+			method:'POST',
+			body: JSON.stringify(postData)
+
+		})
+		.then((response)=> response.json())
+		.then((responseJson)=>{
+			resolve(responseJson);
+		})
+		.catch((error)=>{
+			console.log("request error messge",error);
+			reject(error);
+		});
+		
+	});
+	
+}
